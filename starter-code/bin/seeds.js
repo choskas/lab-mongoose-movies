80cae4e55cf42ee2a9444d14951b9ebb3d81e5ee
Choskas <choskasdelta@hotmail.com>
2019-09-06 20:03:44 -0500

    part2done

---
+++ starter-code/bin/seeds.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose')
+// const Celebrity = require('../models/Celebrity')
+
+// const dbName = 'celebrities'
+// mongoose.connect(`mongodb://localhost/${dbName}`)
+
+
+// const celebrities = [
+//   {
+//     name: "Lady Bitch Ray",
+//     occupation: "Singer",
+//     catchPhrase: "I brought you a gift",
+//   },
+//   {
+//     name: "Cicciolina Ilona Staller",
+//     occupation: "Porn Star and Politician",
+//     catchPhrase: "Oooooh",
+//   },
+//   {
+//     name: "Daniel Craig",
+//     occupation: "Actor",
+//     catchPhrase: "My name is Bond. James Bond.",
+//   },
+
+// ];
+
+// Celebrity.create(celebrities, (err) => {
+//   if (err) {
+//     throw (err)
+//   }
+//   console.log(`${celebrities.length} "celebrities" created.`)
+//   mongoose.connection.close()
+// })
+
+
+const Movie = require('../models/Movie')
+
+const dbName = 'celebrities'
+mongoose.connect(`mongodb://localhost/${dbName}`)
+
+
+const movies = [
+  {
+    title: "just a movie",
+    genre: "comedy",
+    plot: "A really ncie movie",
+  },
+  {
+    title: "just a movie 2",
+    genre: "Horror",
+    plot: "I dont understand this movie but is good",
+  },
+  {
+    title: "WTF movie",
+    genre: "Thriller",
+    plot: "Just a thriller movie that make you say WTF",
+  },
+
+];
+
+Movie.create(movies, (err) => {
+  if (err) {
+    throw (err)
+  }
+  console.log(`${movies.length} "movies" created.`)
+  mongoose.connection.close()
+})
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,130 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Celebrity = require('../models/Celebrity')
+const Movie = require('../models/Movie')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/celebrities/index', (req, res, next) => {
+  Celebrity.find()
+    .then(celebrity => {
+      res.render('celebrities/index', { celebrity })
+    })
+    .catch(error => {
+      console.log('Error while getting the fucking celebrities from the DB: ', error);
+    })
+});
+
+router.get('/celebrities/new', (req, res, next) => {
+  res.render('celebrities/new');
+});
+
+
+router.get('/celebrities/:id', (req, res, next) => {
+  Celebrity.findById({ _id: req.params.id })
+    .then(celebrity => {
+      res.render('celebrities/show', { celebrity })
+    })
+    .catch(error => {
+      console.log('Error while getting the fucking celebrity from the DB: ', error);
+    })
+});
+
+
+router.post('/celebrities', (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body
+  const celebrity = new Celebrity({ name, occupation, catchPhrase })
+  celebrity.save()
+    .then()
+    .catch(error => {
+      console.log('Error occured:', error)
+      res.redirect('/celebrities/new')
+    })
+
+  res.redirect('/celebrities/index')
+  //res.render('index')
+});
+
+
+//MOVIES!!!!!!
+
+router.get('/movies/index', (req, res, next) => {
+  Movie.find()
+    .then(movies => {
+      res.render('movies/index', { movies })
+    })
+    .catch(error => {
+      console.log('Error while getting the fucking movies from the DB: ', error);
+    })
+});
+
+router.get('/movies/new', (req, res, next) => {
+  res.render('movies/new');
+});
+
+router.get('/movies/:id', (req, res, next) => {
+  Movie.findById({ _id: req.params.id })
+    .then(movie => {
+      res.render('movies/show', { movie })
+    })
+    .catch(error => {
+      console.log('Error while getting the fucking movie from the DB: ', error);
+    })
+});
+
+router.post('/movies', (req, res, next) => {
+  const { title, genre, plot } = req.body
+  const movie = new Movie({ title, genre, plot })
+  movie.save()
+    .then()
+    .catch(error => {
+      console.log('Error occured:', error)
+      res.redirect('/movies/new')
+    })
+
+  res.redirect('/movies/index')
+  
+});
+
+
+
+
+router.post('/movies/:id/delete', (req, res, next) => {
+  //const { name, occupation, catchPhrase } = req.body
+  console.log(req.params.id)
+  Movie.findByIdAndRemove({ _id: req.params.id })
+    .then()
+    .catch(error => {
+      console.log('Error occured:', error)
+      res.redirect('/movies/index')
+    })
+
+  res.redirect('/movies/index')
+  //res.render('index')
+});
+
+
+
+
+
+
+
+router.post('/celebrities/:id/delete', (req, res, next) => {
+  //const { name, occupation, catchPhrase } = req.body
+  console.log(req.params.id)
+  Celebrity.findByIdAndRemove({ _id: req.params.id })
+    .then()
+    .catch(error => {
+      console.log('Error occured:', error)
+      res.redirect('/celebrities/index')
+    })
+
+  res.redirect('/celebrities/index')
+  //res.render('index')
+});
+
 
 module.exports = router;
